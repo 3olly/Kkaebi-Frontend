44c7e22fea0965be68f87a5670bc142fa06bc23f
3olly <cy.kim@ewhain.net>
2024-11-25 04:03:29 +0900

    feat: SignupGenerateCodePage 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 import SignupIntroPage from "./pages/SignupPage/SignupIntroPage.jsx";
 import SignupNamePage from "./pages/SignupPage/SignupNamePage.jsx";
 import SignupCodeInputPage from "./pages/SignupPage/SignupCodeInputPage.jsx";
+import SignupSetHomePage from "./pages/SignupPage/SignupSetHomePage.jsx";
+import SignupGenerateCodePage from "./pages/SignupPage/SignupGenerateCodePage.jsx";
 
 function App() {
   function setScreenSize() {
@@ -27,6 +29,11 @@ function App() {
           <Route path="/signupintro" element={<SignupIntroPage />} />
           <Route path="/signupname" element={<SignupNamePage />} />
           <Route path="/signupcodeinput" element={<SignupCodeInputPage />} />
+          <Route path="/signupsethome" element={<SignupSetHomePage />} />
+          <Route
+            path="/signupgeneratecode"
+            element={<SignupGenerateCodePage />}
+          />
         </Routes>
       </Router>
     </>
